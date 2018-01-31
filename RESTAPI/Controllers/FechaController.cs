128f37cd351088f822745cdf6812100d7e0983d6
fjsanchez <fjsanchez.bkb@gmail.com>
2018-01-31 17:46:08 +0100

    vs project added

---
+++ RESTAPI/Controllers/FechaController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class FechaController : Controller
+    {
+        // GET api/fecha
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { System.DateTime.Now.ToLongDateString(), DateTime.Now.TimeOfDay.ToString()};
+        }
+
+        // GET api/fecha/5
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            if(id=="dia")
+            {
+                return DateTime.Now.Day.ToString();
+            }
+            else if (id == "mes")
+            {
+                return DateTime.Now.Month.ToString();
+            }
+            else if (id == "hora")
+            {
+                return DateTime.Now.TimeOfDay.ToString();
+            }
+            else
+            {
+                return "Error...";
+            }
+           
+        }
+
+        //// POST api/fecha
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/fecha/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/fecha/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
